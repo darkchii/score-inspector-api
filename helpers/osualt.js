720045dabf204cf6b49f78f94e64ea1021e3fa1e
Mark Meijer <engineermarkpro@gmail.com>
2022-12-25 01:30:28 +0100

    moar stuff

--- helpers/osualt.js
+++ helpers/osualt.js
@@ -1,6 +1,108 @@
 const { Client } = require("pg");
 require('dotenv').config();
 
+const beatmap_columns = `
+beatmaps.approved, 
+    beatmaps.submit_date, 
+    beatmaps.approved_date, 
+    beatmaps.last_update,
+    beatmaps.artist,
+    beatmaps.set_id,
+    beatmaps.bpm,
+    beatmaps.creator,
+    beatmaps.creator_id,
+    beatmaps.stars,
+    beatmaps.diff_aim,
+    beatmaps.diff_speed,
+    beatmaps.cs,
+    beatmaps.od,
+    beatmaps.ar,
+    beatmaps.hp,
+    beatmaps.drain,
+    beatmaps.source,
+    beatmaps.genre,
+    beatmaps.language,
+    beatmaps.title,
+    beatmaps.length,
+    beatmaps.diffname,
+    beatmaps.file_md5,
+    beatmaps.mode,
+    beatmaps.tags,
+    beatmaps.favorites,
+    beatmaps.rating,
+    beatmaps.playcount,
+    beatmaps.passcount,
+    beatmaps.maxcombo,
+    beatmaps.circles,
+    beatmaps.sliders,
+    beatmaps.spinners,
+    beatmaps.storyboard,
+    beatmaps.video,
+    beatmaps.download_unavailable,
+    beatmaps.audio_unavailable,
+    beatmaps.beatmap_id
+`;
+
+const score_columns = `
+    scores.user_id, 
+    scores.beatmap_id, 
+    scores.score, 
+    scores.count300, 
+    scores.count100, 
+    scores.count50, 
+    scores.countmiss, 
+    scores.combo, 
+    scores.perfect, 
+    scores.enabled_mods, 
+    scores.date_played, 
+    scores.rank, 
+    scores.pp, 
+    scores.accuracy, 
+    ${beatmap_columns},
+    moddedsr.star_rating,
+    moddedsr.aim_diff,
+    moddedsr.speed_diff,
+    moddedsr.fl_diff,
+    moddedsr.slider_factor,
+    moddedsr.speed_note_count,
+    moddedsr.modded_od,
+    moddedsr.modded_ar,
+    moddedsr.modded_cs,
+    moddedsr.modded_hp
+`;
+
+const score_columns_full = `
+    scores.user_id, 
+    scores.beatmap_id, 
+    scores.score, 
+    scores.count300, 
+    scores.count100, 
+    scores.count50, 
+    scores.countmiss, 
+    scores.combo, 
+    scores.perfect, 
+    scores.enabled_mods, 
+    scores.date_played, 
+    scores.rank, 
+    scores.pp, 
+    scores.accuracy, 
+    ${beatmap_columns},
+    moddedsr.star_rating,
+    moddedsr.aim_diff,
+    moddedsr.speed_diff,
+    moddedsr.fl_diff,
+    moddedsr.slider_factor,
+    moddedsr.speed_note_count,
+    moddedsr.modded_od,
+    moddedsr.modded_ar,
+    moddedsr.modded_cs,
+    moddedsr.modded_hp,
+    pack_id
+    `;
+module.exports.score_columns = score_columns;
+module.exports.beatmap_columns = beatmap_columns;
+module.exports.score_columns_full = score_columns_full;
+
 module.exports.IsRegistered = IsRegistered;
 async function IsRegistered(id) {
     let data;
@@ -40,9 +142,9 @@ async function GetUser(id) {
         await client.connect();
         const { rows } = await client.query(`SELECT * FROM users2 WHERE user_id=$1`, [id]);
         await client.end();
-        if(rows.length > 0){
+        if (rows.length > 0) {
             data = rows[0];
-        }else{
+        } else {
             throw new Error('User not found');
         }
     } catch (err) {
@@ -77,6 +179,46 @@ async function FindUser(query, single) {
             data = rows;
         }
     } catch (err) {
+        throw new Error(err.message);
+    }
+    return data;
+}
+
+module.exports.GetBestScores = GetBestScores;
+async function GetBestScores(period, stat, limit, loved = false) {
+    let data;
+    try {
+        let period_check = null;
+        switch (period) {
+            case 'day':
+                period_check = 1;
+                break;
+            case 'week':
+                period_check = 7;
+                break;
+            case 'month':
+                period_check = 31;
+                break;
+            case 'year':
+                period_check = 365;
+                break;
+            case 'all':
+                period_check = null;
+                break;
+        }
+        const client = new Client({ user: process.env.ALT_DB_USER, host: process.env.ALT_DB_HOST, database: process.env.ALT_DB_DATABASE, password: process.env.ALT_DB_PASSWORD, port: process.env.ALT_DB_PORT });
+        await client.connect();
+        const approved_query = `(beatmaps.approved = 1 OR beatmaps.approved = 2 ${loved ? 'OR beatmaps.approved = 4' : ''})`;
+        const { rows } = await client.query(`
+        SELECT ${score_columns}, users2.pp as user_pp, users2.username FROM scores 
+        LEFT JOIN beatmaps ON scores.beatmap_id = beatmaps.beatmap_id
+        LEFT JOIN moddedsr on beatmaps.beatmap_id = moddedsr.beatmap_id and moddedsr.mods_enum = (case when is_ht = 'true' then 256 else 0 end + case when is_dt = 'true' then 64 else 0 end + case when is_hr = 'true' then 16 else 0 end + case when is_ez = 'true' then 2 else 0 end + case when is_fl = 'true' then 1024 else 0 end) 
+        INNER JOIN users2 ON scores.user_id = users2.user_id 
+        WHERE ${period_check !== null ? `date_played > current_date - ${period_check} AND ` : ''} ${approved_query} ORDER BY scores.${stat} DESC LIMIT $1`, [limit]);
+        await client.end();
+        data = rows;
+    } catch (err) {
+        console.error(err);
         throw new Error(err.message);
     }
     return data;
--- routes/scores.js
+++ routes/scores.js
@@ -1,89 +1,109 @@
 var express = require('express');
 var router = express.Router();
+var apicache = require('apicache');
 const { Client } = require('pg');
+const { GetBestScores, score_columns, score_columns_full, beatmap_columns } = require('../helpers/osualt');
 require('dotenv').config();
 
-const score_columns = `
-    scores.user_id, 
-    scores.beatmap_id, 
-    scores.score, 
-    scores.count300, 
-    scores.count100, 
-    scores.count50, 
-    scores.countmiss, 
-    scores.combo, 
-    scores.perfect, 
-    scores.enabled_mods, 
-    scores.date_played, 
-    scores.rank, 
-    scores.pp, 
-    scores.accuracy, 
-    beatmaps.approved, 
-    beatmaps.submit_date, 
-    beatmaps.approved_date, 
-    beatmaps.last_update,
-    beatmaps.artist,
-    beatmaps.set_id,
-    beatmaps.bpm,
-    beatmaps.creator,
-    beatmaps.creator_id,
-    beatmaps.stars,
-    beatmaps.diff_aim,
-    beatmaps.diff_speed,
-    beatmaps.cs,
-    beatmaps.od,
-    beatmaps.ar,
-    beatmaps.hp,
-    beatmaps.drain,
-    beatmaps.source,
-    beatmaps.genre,
-    beatmaps.language,
-    beatmaps.title,
-    beatmaps.length,
-    beatmaps.diffname,
-    beatmaps.file_md5,
-    beatmaps.mode,
-    beatmaps.tags,
-    beatmaps.favorites,
-    beatmaps.rating,
-    beatmaps.playcount,
-    beatmaps.passcount,
-    beatmaps.maxcombo,
-    beatmaps.circles,
-    beatmaps.sliders,
-    beatmaps.spinners,
-    beatmaps.storyboard,
-    beatmaps.video,
-    beatmaps.download_unavailable,
-    beatmaps.audio_unavailable,
-    moddedsr.star_rating,
-    moddedsr.aim_diff,
-    moddedsr.speed_diff,
-    moddedsr.fl_diff,
-    moddedsr.slider_factor,
-    moddedsr.speed_note_count,
-    moddedsr.modded_od,
-    moddedsr.modded_ar,
-    moddedsr.modded_cs,
-    moddedsr.modded_hp,
-    pack_id
-    `;
-
-// pack_id
+let cache = apicache.middleware;
 
 /* Get the entire list of scores of a user */
-router.get('/:id', async function (req, res, next) {
+router.get('/user/:id', async function (req, res, next) {
     const client = new Client({ user: process.env.ALT_DB_USER, host: process.env.ALT_DB_HOST, database: process.env.ALT_DB_DATABASE, password: process.env.ALT_DB_PASSWORD, port: process.env.ALT_DB_PORT });
     await client.connect();
     const approved_query = `AND (beatmaps.approved = 1 OR beatmaps.approved = 2 ${req.query.loved === 'true' ? 'OR beatmaps.approved = 4' : ''})`;
     const { rows } = await client.query(`
-        SELECT ${score_columns} FROM scores 
+        SELECT ${score_columns_full}, users2.username FROM scores 
         LEFT JOIN beatmaps ON scores.beatmap_id = beatmaps.beatmap_id 
         LEFT JOIN moddedsr on beatmaps.beatmap_id = moddedsr.beatmap_id and moddedsr.mods_enum = (case when is_ht = 'true' then 256 else 0 end + case when is_dt = 'true' then 64 else 0 end + case when is_hr = 'true' then 16 else 0 end + case when is_ez = 'true' then 2 else 0 end + case when is_fl = 'true' then 1024 else 0 end) 
         LEFT join (select beatmap_id, STRING_AGG(pack_id, ',') as pack_id from beatmap_packs group by beatmap_id) bp on beatmaps.beatmap_id = bp.beatmap_id 
-        WHERE user_id=$1 ${approved_query}`, [req.params.id]);
+        INNER JOIN users2 ON scores.user_id = users2.user_id 
+        WHERE scores.user_id=$1 ${approved_query}`, [req.params.id]);
     await client.end();
     res.json(rows);
+});
+
+const valid_periods = ['all', 'year', 'month', 'week', 'day'];
+const valid_stats = ['pp', 'score'];
+router.get('/best', cache('1 hour'), async function (req, res, next) {
+    const period = req.query.period || 'all';
+    const stat = req.query.stat || 'pp';
+    const limit = req.query.limit || 5;
+    const loved = req.query.loved ? true : false;
+    if (!valid_periods.includes(period)) {
+        res.status(400).json({ "error": "Invalid period" });
+        return;
+    }
+    if (!valid_stats.includes(stat)) {
+        res.status(400).json({ "error": "Invalid stat" });
+        return;
+    }
+    if (isNaN(limit) || limit < 1 || limit > 100) {
+        res.status(400).json({ "error": "Invalid limit" });
+        return;
+    }
+
+    let scores;
+    try {
+        scores = await GetBestScores(period, stat, limit, loved);
+    } catch (e) {
+        res.status(500).json({ "error": "Error while fetching scores" });
+        return;
+    }
+
+    res.json(scores);
+});
+
+const STAT_PERIODS = [
+    '24h', 'all'
+]
+router.get('/stats', cache('1 hour'), async function (req, res, next) {
+    //stats from today
+    const client = new Client({ user: process.env.ALT_DB_USER, host: process.env.ALT_DB_HOST, database: process.env.ALT_DB_DATABASE, password: process.env.ALT_DB_PASSWORD, port: process.env.ALT_DB_PORT });
+    await client.connect();
+    const approved_query = `(beatmaps.approved = 1 OR beatmaps.approved = 2 OR beatmaps.approved = 4)`;
+    const join_query = `
+    LEFT JOIN beatmaps ON scores.beatmap_id = beatmaps.beatmap_id 
+    INNER JOIN users2 ON scores.user_id = users2.user_id
+    `;
+    const full_query = `SELECT 
+        count(*) as scores, 
+        sum(score) as total_score,
+        sum(case when scores.pp = 'NaN' then 0 else scores.pp end) as total_pp,
+        max(case when scores.pp = 'NaN' then 0 else scores.pp end) as max_pp,
+        sum(length) as total_length,
+        count(*) FILTER (WHERE rank = 'XH') as scores_xh,
+        count(*) FILTER (WHERE rank = 'X') as scores_x,
+        count(*) FILTER (WHERE rank = 'SH') as scores_sh,
+        count(*) FILTER (WHERE rank = 'S') as scores_s,
+        count(*) FILTER (WHERE rank = 'A') as scores_a,
+        count(*) FILTER (WHERE rank = 'B') as scores_b,
+        count(*) FILTER (WHERE rank = 'C') as scores_c,
+        count(*) FILTER (WHERE rank = 'D') as scores_d,
+        sum(count300+count100+count50) as total_hits
+    FROM scores 
+    ${join_query} 
+    WHERE ${approved_query}`;
+
+    let _res = {};
+    for await (const period of STAT_PERIODS) {
+        let time_query = '';
+        if (period === '24h') {
+            time_query = 'AND (date_played BETWEEN NOW() - INTERVAL \'24 HOURS\' AND NOW())';
+        } else if (period === '7d') {
+            time_query = 'AND (date_played BETWEEN NOW() - INTERVAL \'7 DAYS\' AND NOW())';
+        }
+        const { rows } = await client.query(`${full_query} ${time_query}`);
+
+        const most_played_map_columns = beatmap_columns;
+        const { rows: most_played_map } = await client.query(`SELECT count(*), ${most_played_map_columns} FROM scores ${join_query} WHERE ${approved_query} ${time_query} GROUP BY ${most_played_map_columns} ORDER BY count(*) DESC LIMIT 1`);
+        let data = rows[0];
+        data.most_played_map = most_played_map[0];
+        _res[period] = data;
+    }
+
+    await client.end();
+    res.json(_res);
 });
 
 module.exports = router;

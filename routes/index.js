fff6812afc2045bcbcabfca36e0cbd682d377c1e
Mark Meijer <engineermarkpro@gmail.com>
2023-01-28 00:47:00 +0100

    Fixed proxy endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
+const { default: axios } = require('axios');
 var express = require('express');
+const request = require('request');
 const { RankTest } = require('../helpers/osu');
 var router = express.Router();
 require('dotenv').config();
 
 router.get('/proxy/:url', async (req, res) => {
-  const url = Buffer.from(req.params.url, 'base64').toString('utf-8');
-  const _res = await axios.get(url);
-
-  res.json(_res.data);
+  try{
+    const url = Buffer.from(req.params.url, 'base64').toString('utf-8');
+    // const _res = await axios.get(url);
+    request.get(url).pipe(res);
+  }catch(err){
+    res.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = router;

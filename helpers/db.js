232d3b00648f3461d827e7fd600af6f4daa0ae72
Mark <engineermarkpro@gmail.com>
2024-06-19 14:02:16 +0200

    add db max retry of 10 times

--- helpers/db.js
+++ helpers/db.js
@@ -35,8 +35,31 @@ const { InspectorClanStatsModel } = require('./models/InspectorClanStats.js');
 require('dotenv').config();
 
 let databases = {
-    inspector: new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASS, { host: process.env.MYSQL_HOST, dialect: 'mariadb', timezone: 'Europe/Amsterdam', logging: false }),
-    osuAlt: new Sequelize(process.env.ALT_DB_DATABASE, process.env.ALT_DB_USER, process.env.ALT_DB_PASSWORD, { host: process.env.ALT_DB_HOST, dialect: 'postgres', logging: false })
+    inspector: new Sequelize(
+        process.env.MYSQL_DB, 
+        process.env.MYSQL_USER, 
+        process.env.MYSQL_PASS, 
+        { 
+            host: process.env.MYSQL_HOST, 
+            dialect: 'mariadb', 
+            timezone: 'Europe/Amsterdam', 
+            logging: false,
+            retry: {
+                max: 10
+            }
+        }),
+    osuAlt: new Sequelize(
+        process.env.ALT_DB_DATABASE, 
+        process.env.ALT_DB_USER, 
+        process.env.ALT_DB_PASSWORD, 
+        { 
+            host: process.env.ALT_DB_HOST, 
+            dialect: 'postgres', 
+            logging: false,
+            retry: {
+                max: 10
+            }
+        })
 };
 module.exports.Databases = databases;
 
